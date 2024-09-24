86126ce34dca9a0292a1d2d3bcf5326fb5ce2e1d
TheBlueShuckle <villehalldin05@gmail.com>
2024-09-24 09:49:48 +0200

    Complete v1

--- flippblipp-v1.py
+++ flippblipp-v1.py
@@ -1,2 +1,24 @@
 #input desired n
-n = input("Choose a length of the game: ")
+n = int(input("Choose a length of the game: "))
+
+def isDivisibleByThree(number):
+    return number % 3 == 0
+
+def isDivisibleByFive(number):
+    return number % 5 == 0
+
+
+for x in range(n):
+    number = x + 1
+    
+    if (isDivisibleByThree(number) and isDivisibleByFive(number)):
+        print("flipp blipp")
+        
+    elif (isDivisibleByFive(number)):
+        print("blipp")
+        
+    elif (isDivisibleByThree(number)):
+        print("flipp")
+    
+    else:
+        print(number)
