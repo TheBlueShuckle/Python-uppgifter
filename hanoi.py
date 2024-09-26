c93d64e44e63602ada923e938d02b704c02f5f7b
coraxites <coraxites@gmail.com>
2024-09-26 09:47:43 +0200

    the hanoi shuffle

--- hanoi.py
+++ hanoi.py
@@ -1,14 +1,26 @@
 # Hanoi
-peg_a = [4, 3, 2, 1]
-peg_b = []
-peg_c = []
+A = [4, 3, 2, 1]
+B = []
+C = []
 
 def hanoi(n, src, dest, aux):
-    print('')
+    move(src, aux)
+    move(src, dest)
+    move(aux, dest)
+    hanoi(n, A, B, C)
+
+def move(src, dest):
+    temp = src[len(src)-1]
+    src.pop(len(src)-1)
+    dest.append(temp)
+    render(A, B, C)
 
 def render(peg_a, peg_b, peg_c):
     print('A', peg_a)
     print('B', peg_b)
     print('C', peg_c)
+    print('-')
 
-render(peg_a, peg_b, peg_c)
+render(A, B, C)
+
+hanoi(2, A, B, C)
