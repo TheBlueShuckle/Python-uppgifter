2d2c5f23400b5343c0330c1e5a4dbd7aa0733b82
coraxites <coraxites@gmail.com>
2024-09-26 08:47:21 +0200

    render complete

--- hanoi.py
+++ hanoi.py
@@ -1,15 +1,14 @@
 # Hanoi
-peg_a = [1, 2, 3, 4]
+peg_a = [4, 3, 2, 1]
 peg_b = []
 peg_c = []
 
 def hanoi(n, src, dest, aux):
-    
+    print('')
 
 def render(peg_a, peg_b, peg_c):
-    for n in peg_a:
-        print('A', peg_a(n))
-    for n in peg_b:
-        print('B', peg_b(n))
-    for n in peg_c:
-        print('C', peg_c(n))
+    print('A', peg_a)
+    print('B', peg_b)
+    print('C', peg_c)
+
+render(peg_a, peg_b, peg_c)
