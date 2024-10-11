fab24dbacacaed2406d618850162a32c31b7dd75
TheBlueShuckle <villehalldin05@gmail.com>
2024-10-11 13:52:41 +0200

    Save users

--- lagra.py
+++ lagra.py
@@ -1,23 +1,45 @@
+import os
+
 users = {
-    "Foo" : "Bar"
+    "Foo" : "Bar",
+    "FooBar" : "Baz" 
 }
 
-def check_login(username, password):
-    if(len(users) == 0):
-        return False
+user_items = ["Pen", "Laptop", "Eraser"]
 
-    if (users.get(username) == password):
-        return True
+def save_user_info(username, user_items):
+    path = 'usrs/' + username
+    user_file = ""
+    fileExists = os.path.exists(path)
+
+    if (not fileExists):
+        user_file = open(path, 'x')
+        user_file.close()
+
+    user_file = open(path, 'w')
+
+    for item in user_items:
+        user_file.write(item + "\n")
+
+
+# def check_login(username, password):
+#     if(len(users) == 0):
+#         return False
+
+#     if (users.get(username) == password):
+#         return True
     
-    return False
+#     return False
 
+# # test - delete later
+# username = input("Input Username: ")
+# password = input("Input Password: ")
 
-# test - delete later
-username = input("Input Username: ")
-password = input("Input Password: ")
+# if (check_login(username, password)):
+#     print("Logging in")
 
-if (check_login(username, password)):
-    print("Logging in")
+# else:
+#     print("Incorrect username or password")
 
-else:
-    print("Incorrect username or password")
+# save_user_info("Foo", user_items)
+# save_user_info("FooBar", user_items)
