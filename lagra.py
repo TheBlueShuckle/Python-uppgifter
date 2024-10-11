cab5786d7a2562b0c79ee4f909c3c5b0d40dedb8
coraxites <coraxites@gmail.com>
2024-10-11 13:53:08 +0200

    add ui_login and ui_user

--- lagra.py
+++ lagra.py
@@ -1,19 +1,56 @@
-def index():
-    print('Welcome to Lagra (TM)\n')
-    print('l) Log in')
-    print('q) Quit\n')
-    option = input_loop(['l', 'q'])
-    match i:
-        case 'l':
-            login()
-        case 'q':
-            print('Thank you for using Lagra (TM)!')
-
 def input_loop(options):
     # options is a list of strings
     i = ''
     while i not in options:
-        i = input('\nOption: ')
+        i = input('Option: ')
     return i
 
-index()
+def ui_index():
+    print('Welcome to Lagra (TM)')
+    print('\nl) Log in')
+    print('q) Quit\n')
+    option = input_loop(['l', 'q'])
+    match option:
+        case 'l':
+            ui_login()
+        case 'q':
+            pass
+    print('Thank you for using Lagra (TM)!')
+
+def ui_login():
+    while True:
+        username = input('    User: ')
+        password = input('Password: ')
+        login_ok = check_login(username, password)
+        match login_ok:
+            case True:
+                print('temp')
+                # Switch to logged in user
+            case False:
+                print('Invalid username or password')
+                print('\nr) Try again')
+                print('q) Quit\n')
+                option = input_loop(['r', 'q'])
+                match option:
+                    case 'r':
+                        continue
+                    case 'q':
+                        break
+
+def ui_user(user):
+    print('Welcome' + user)
+    list_items()
+
+    while True:
+        print('Select an action')
+        print('\na) Add item')
+        print('l) List items')
+        print('q) Log out')
+        option = input_loop(['a', 'l', 'q'])
+        match option:
+            case 'a':
+                add_item()
+            case 'l':
+                list_items()
+            case 'q':
+                break
