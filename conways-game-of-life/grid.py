bb018bd7b5216bf8330c8e83fc209eda5de50c8f
TheBlueShuckle <villehalldin05@gmail.com>
2024-10-15 14:40:14 +0200

    idk man

--- conways-game-of-life/grid.py
+++ conways-game-of-life/grid.py
@@ -3,40 +3,15 @@ from tile import Tile
 class Grid:
     tiles = []  
 
-    def __init__(self, pixels_x, pixels_y, pixel_size):
-        self.pixels_x = pixels_x
-        self.pixels_y = pixels_y
-        self.pixel_size = pixel_size   
+    def __init__(self, tiles_x, tiles_y, pixel_size):
+        self.pixels_x = tiles_x
+        self.pixels_y = tiles_y
+        self.tile_size = pixel_size   
 
-        self.tiles = [[Tile(x, y, pixel_size, False, False) for x in range(pixels_x) for y in range(pixels_y)]]
+        self.tiles = [[Tile(x, y, pixel_size, False, False) for x in range(tiles_x) for y in range(tiles_y)]]
 
-    def get_neighbors(self, tile):
-        neighbors = []
-
-
-        # private Point[] GetNeighbors(int[,] map, Point point)
-        # {
-        #     List<Point> neighbors = new List<Point>();
-
-        #     if (map.GetLength(0) - 1 != point.X)
-        #     {
-        #         neighbors.Add(new Point(point.X + 1, point.Y));
-        #     }
-
-        #     if (point.X != 0)
-        #     {
-        #         neighbors.Add(new Point(point.X - 1, point.Y));
-        #     }
-
-        #     if (map.GetLength(1) - 1 != point.Y)
-        #     {
-        #         neighbors.Add(new Point(point.X, point.Y + 1));
-        #     }
-
-        #     if (point.Y != 0)
-        #     {
-        #         neighbors.Add(new Point(point.X, point.Y -  1));
-        #     }
-
-        #     return neighbors.ToArray();
-        # }
+    def set_neighbors(self, tile):
+        for y in range(self.pixels_y):
+            for x in range(self.pixels_x):
+                neighbors = []
+                
