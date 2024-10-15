f72f3da52826e25f08276c1ba4e6b3bdaddb210b
TheBlueShuckle <villehalldin05@gmail.com>
2024-10-15 20:03:52 +0200

    Fix finding neighbors

--- conways-game-of-life/grid.py
+++ conways-game-of-life/grid.py
@@ -8,14 +8,14 @@ class Grid:
         self.tiles_y = tiles_y
         self.tile_size = tile_size   
 
-        self.tiles = [[Tile(x, y, tile_size, False, False) for x in range(tiles_x) for y in range(tiles_y)]]
+        self.tiles = [[Tile(x, y, tile_size, False, False) for x in range(tiles_x)] for y in range(tiles_y)]
 
     def get_neighbors(self, tile):
         #              (-1,-1), (0,-1), (1,-1)
         #              (-1, 0), (0, 0), (1, 0)
         #              (-1, 1), (0, 1), (1, 1)
-        x_offsets = { -1, 0, 1, 1, 1, 0, -1, -1 }
-        y_offsets = { -1, -1, -1, 0, 1, 1, 1, 0 }
+        x_offsets = [ -1, 0, 1, 1, 1, 0, -1, -1 ]
+        y_offsets = [ -1, -1, -1, 0, 1, 1, 1, 0 ]
         max_x = self.tiles_x
         max_y = self.tiles_y
         neighbors = []
@@ -24,9 +24,14 @@ class Grid:
             if not (tile.raw_x + x_offsets[pos] < 0) or not (tile.raw_x + x_offsets[pos] > max_x) and not (tile.raw_y + y_offsets[pos] < 0) or not (tile.raw_y + y_offsets[pos] > max_y):
                 neighbors.append(self.tiles[tile.raw_x + x_offsets[pos]][tile.raw_y + y_offsets[pos]])
 
-        return [neighbors]
+        return neighbors
 
     def set_neighbors(self):
         for y in range(self.pixels_y):
             for x in range(self.pixels_x):
                 self.tiles[x][y].neighbors = self.get_neighbors(self.tiles[x][y])
+
+grid = Grid(100, 100, 8)
+
+for neighbor in grid.get_neighbors(grid.tiles[10][10]):
+    print(str(neighbor.x) + " " + str(neighbor.y))
