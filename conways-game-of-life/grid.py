862dd1408bf3a440a6446ba33980bdc364799caa
coraxites <coraxites@gmail.com>
2024-10-15 16:40:41 +0200

    mashing together rock

--- conways-game-of-life/grid.py
+++ conways-game-of-life/grid.py
@@ -6,7 +6,7 @@ class Grid:
     def __init__(self, tiles_x, tiles_y, pixel_size):
         self.pixels_x = tiles_x
         self.pixels_y = tiles_y
-        self.tile_size = pixel_size   
+        self.tile_size = pixel_size
 
         self.tiles = [[Tile(x, y, pixel_size, False, False) for x in range(tiles_x) for y in range(tiles_y)]]
 
