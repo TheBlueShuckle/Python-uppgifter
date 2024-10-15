52516f11aca651e75c799942dea0a4701ddd359e
TheBlueShuckle <villehalldin05@gmail.com>
2024-10-15 15:46:13 +0200

    pls work

--- conways-game-of-life/tile.py
+++ conways-game-of-life/tile.py
@@ -1,7 +1,11 @@
 class Tile:
     neighbors = []
+    raw_x = 0
+    raw_y = 0
 
     def __init__(self, x, y, size, alive, hover):
+        self.raw_x = x
+        self.raw_y = y
         self.x = x * size #int
         self.y = y * size #int
         self.size = size #int
