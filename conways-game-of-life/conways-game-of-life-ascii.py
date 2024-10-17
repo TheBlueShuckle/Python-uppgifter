b9e79e3f71f70dbb92533ec8ba3ad856cb1a2fec
coraxites <coraxites@gmail.com>
2024-10-17 16:32:24 +0200

    display functioning

--- conways-game-of-life/conways-game-of-life-ascii.py
+++ conways-game-of-life/conways-game-of-life-ascii.py
@@ -2,3 +2,20 @@ from tile import Tile
 from grid import Grid
 
 grid = Grid(10, 10, 0)
+
+def raw_print(string):
+    print(string, end='')
+
+def display_grid(grid):
+    print('  0 1 2 3 4 5 6 7 8 9')
+    for row in grid.tiles:
+        raw_print(grid.tiles.index(row))
+        for cell in row:
+            match cell.isAlive:
+                case True:
+                    raw_print('# ')
+                case False:
+                    raw_print('  ')
+        raw_print('\n')
+
+display_grid(grid)
