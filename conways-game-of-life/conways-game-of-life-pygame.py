27d11db686b00ac99b820f4484dc56c973333440
coraxites <coraxites@gmail.com>
2024-10-21 14:59:18 +0200

    looks good

--- conways-game-of-life/conways-game-of-life-pygame.py
+++ conways-game-of-life/conways-game-of-life-pygame.py
@@ -75,27 +75,50 @@ def dehover(hovered_tile):
 # // PYGAME START //
 
 pygame.init()
+pygame.font.init()
+font = pygame.font.SysFont('Roboto', 30)
+text_instructions = font.render('Press R to start simulation', True, (255, 255, 255))
 
 grid = Grid(100, 100, 8)
 pygame.mouse.set_visible(False) # No more mouse-centering problems if you can't see the mouse -C
+mouse_down = False
+generation = 0
+sim_active = False
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill('black')
-    
+
     mouse_x, mouse_y = get_cursor_pos()
     hovered_tile = hover(mouse_x, mouse_y)
     draw_grid(grid.tiles) # Passing in grid.tiles because its more efficient (dont cite me) -V
 
+    if sim_active:
+        text_counter = font.render(str(generation), True, (255, 255, 255))
+        screen.blit(text_counter, (0,0))
+        simulation(grid)
+        generation += 1
+    else:
+        screen.blit(text_instructions, (0,0))
+
+    if mouse_down == True:
+        hovered_tile.flip()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_down = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            mouse_down = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                sim_active = True
+
     pygame.display.flip()
     dehover(hovered_tile)
 
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(30)  # limits FPS to 30
 
 pygame.quit()
